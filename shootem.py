786c9b2d7da2f7d7746a8408a02a4706f9bc17c0
dylanptcu <dylan.perez@tcu.edu>
2020-03-21 14:46:58 -0500

    started creating class and functions

--- shootem.py
+++ shootem.py
@@ -27,14 +27,42 @@ nummags = count(root[1])
 # get body part count
 numbodyparts = count(root[2])
 
-# set random gun text
-gun = root[0][r.randint(1,numguns)-1][0].text
-# set random body part text
-bodypart = root[2][r.randint(1,numbodyparts)-1].text
-# get capacity of random magazine
-magazine = root[1][r.randint(1,nummags)-1][1].text
-# get random shot count
-shotcount = r.randint(1,int(magazine))
 
-# print shot statement
-print(f'you were shot with a {gun} in the {bodypart} {shotcount} times.')
+class Shootem():
+
+    def __init__(self):
+        self.gun = root[0][r.randint[1, numguns-1]]
+        self.mag = root[1][r.randint(1,nummags)-1]
+        self.count = 0
+
+    def getgun(self):
+        name = self.gun[0]
+        return name
+
+    def getmag(self):
+        magazine = self.mag[0]
+        if self.gun[1] == "manual":
+            return ""
+        else:
+            return magazine
+
+    def getcount(self):
+
+
+
+
+
+
+
+sh = Shootem()
+
+print(sh.selectgun())
+# # set random body part text
+# bodypart = root[2][r.randint(1,numbodyparts)-1].text
+# # get capacity of random magazine
+# magazine = root[1][r.randint(1,nummags)-1][1].text
+# # get random shot count
+# shotcount = r.randint(1,int(magazine))
+#
+# # print shot statement
+# print(f'you were shot with a {gun} in the {bodypart} {shotcount} times.')
