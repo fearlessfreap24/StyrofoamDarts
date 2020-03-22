230edd71b72488fe9b668e7b3b300bb18a4cab50
dylanptcu <dylan.perez@tcu.edu>
2020-03-22 13:09:16 -0500

    added comments and cleaned up some code

--- shootem.py
+++ shootem.py
@@ -30,39 +30,44 @@ numbodyparts = count(root[2])
 
 class Shootem:
 
+    # standard init - defining the items that wil lbe used to shoot
     def __init__(self):
         self.gun = root[0][r.randint(1, numguns-1)]
         self.mag = root[1][r.randint(1, nummags-1)]
         self.body = root[2][r.randint(1, numbodyparts-1)]
 
     def getgun(self):
-        name = self.gun[0].text
-        return name
+        # get the name of the weapon chosen
+        return self.gun[0].text
 
     def getmag(self):
-        magazine = self.mag[0].text
-        if self.gun[1].text == "manual":
-            return ""
-        else:
-            return magazine
+        # return name of magazine
+        return self.mag[0].text
 
     def getcount(self):
+        # check if weapon is manual feed
         if self.gun[1].text == "manual":
+            # return maxCap of weapon if manual feed
             return self.gun[2].text
         else:
+            # return magazine cap
             return self.mag[1].text
 
     def getbodypart(self):
+        # return name of body part
         return self.body.text
 
     def printshoot(self, author):
+        # create variables for shoot statement
         gun = self.getgun()
         mag = self.getmag()
         cnt = self.getcount()
         body = self.getbodypart()
 
-        if mag == "":
-            return f'{author} was shot in the {body} {r.randint(1, int(cnt))} times with a {gun} .'
+        # shoot statement for manual feed weapons
+        if self.gun[1].text == "manual":
+            return f'@{author} was shot in the {body} {r.randint(1, int(cnt))} times with a {gun} .'
+        # shoot statement for magazine feed weapons
         else:
-            return f'{author} was shot in the {body} {r.randint(1, int(cnt))} times with a {gun}' \
+            return f'@{author} was shot in the {body} {r.randint(1, int(cnt))} times with a {gun}' \
                    f' holding a {mag} magazine .'
